0b6a58f5a61f2d953eaaab63cac26b679d5114cf
Tibor Vass <teabee89@gmail.com>
2025-06-05 00:24:02 -0700

    merge: add required arg
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- cmd/container-use/merge.go
+++ cmd/container-use/merge.go
@@ -10,8 +10,9 @@ import (
 )
 
 var mergeCmd = &cobra.Command{
-	Use:   "merge",
+	Use:   "merge <env>",
 	Short: "Merges an environment into the current git branch",
+	Args:  cobra.ExactArgs(1),
 	RunE: func(app *cobra.Command, args []string) error {
 		env := args[0]
 		// prevent accidental single quotes to mess up command
