a9fdfb58a2d4c31d428135c139f4444ef57dcb8b
Tibor Vass <teabee89@gmail.com>
2025-06-04 19:13:14 -0700

    cmd: Add "merge" to git merge an environment
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

---
+++ cmd/container-use/merge.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var mergeCmd = &cobra.Command{
+	Use:   "merge",
+	Short: "Merges an environment into the current git branch",
+	RunE: func(app *cobra.Command, args []string) error {
+		env := args[0]
+		// prevent accidental single quotes to mess up command
+		env = strings.Trim(env, "'")
+		cmd := exec.CommandContext(app.Context(), "bash", "-c", fmt.Sprintf("git stash --include-untracked && git merge -m 'Merge environment %s' -- %q && git stash pop", env, "container-use/"+env))
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+
+		return cmd.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(mergeCmd)
+}
