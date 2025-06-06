2e2489e2e5372bef236ea3788b9b959d86552e89
Jason McCallister <jason@mccallister.dev>
2025-06-06 12:06:05 -0400

    check if the watch command is installed
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- cmd/cu/watch.go
+++ cmd/cu/watch.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 
@@ -12,6 +13,11 @@ var watchCmd = &cobra.Command{
 	Short: "Watch git log output",
 	Long:  `Watch git log output using the watch command: 'watch --color -n1 git log --color=always --remotes=container-use --oneline --graph --decorate'.`,
 	RunE: func(app *cobra.Command, _ []string) error {
+		// check if the watch command is available
+		if _, err := exec.LookPath("watch"); err != nil {
+			return fmt.Errorf("the 'watch' command is not available. Please install it to use this feature: %w", err)
+		}
+
 		cmd := exec.CommandContext(app.Context(), "watch", "--color", "-n1", "git", "log", "--color=always", "--remotes=container-use", "--oneline", "--graph", "--decorate")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
