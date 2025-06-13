ffda38dabc9f80ca35c1e332f9e415d4d2284f19
Connor Braa <3478454+cwlbraa@users.noreply.github.com>
2025-06-13 14:07:06 -0700

    add cu version command (#69)
    * add cu version command
    
    Signed-off-by: Connor Braa <connor@dagger.io>
    
    * use buildinfo for 'go build' dev binaries
    
    Signed-off-by: Connor Braa <connor@dagger.io>
    
    * only print commit and built if they're known
    
    Signed-off-by: Connor Braa <connor@dagger.io>
    
    ---------
    
    Signed-off-by: Connor Braa <connor@dagger.io>

---
+++ cmd/cu/version.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Long:  `Print the version, commit hash, and build date of the cu binary.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		currentVersion := version
+		currentCommit := commit
+		currentDate := date
+
+		// For dev builds, try to extract build info from the binary
+		if version == "dev" {
+			if buildCommit, buildTime := getBuildInfoFromBinary(); buildCommit != "unknown" {
+				currentCommit = buildCommit
+				currentDate = buildTime
+			}
+		}
+
+		fmt.Printf("cu version %s\n", currentVersion)
+		if currentCommit != "unknown" {
+			fmt.Printf("commit: %s\n", currentCommit)
+		}
+		if currentDate != "unknown" {
+			fmt.Printf("built: %s\n", currentDate)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func getBuildInfoFromBinary() (string, string) {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown", "unknown"
+	}
+
+	var revision, buildTime, modified string
+
+	for _, setting := range buildInfo.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			revision = setting.Value
+		case "vcs.time":
+			buildTime = setting.Value
+		case "vcs.modified":
+			modified = setting.Value
+		}
+	}
+
+	// Format commit hash (use short version)
+	if len(revision) > 7 {
+		revision = revision[:7]
+	}
+	if modified == "true" {
+		revision += "-dirty"
+	}
+
+	if revision == "" {
+		revision = "unknown"
+	}
+	if buildTime == "" {
+		buildTime = "unknown"
+	}
+
+	return revision, buildTime
+}
